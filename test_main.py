0b63e1c0469ae65b8d9380c315535c688bb66be0
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-04-02 20:48:29 +0800

    force autograding reload from upstream

--- test_main.py
+++ test_main.py
@@ -1,27 +1,49 @@
-import contextlib
+from importlib import reload
 import subprocess
 import sys
 import unittest
+
+import main
+
 
 # limit number of retries
 MAX_RETRIES = 5
 retries = 0
 
-# Force reload of autograding submodule
-# This allows it to be updated even after students have accepted an assignment
-# It would otherwise require them to delete and re-accept the assignment
-# (or alternatively update the submodule manually)
-def unload():
-    global autograding
-    if "autograding" in sys.modules:
-        del sys.modules["autograding"]
-    with contextlib.suppress(NameError):
-        del autograding
 
-unload()
-subprocess.run(["git", "submodule", "update", "--init", "--remote"])
-import autograding
-from autograding.case import FuncCall, InOut
+def fetch_autograding() -> None:
+    subprocess.run(["git", "submodule", "update", "--init", "--remote"])
+    
+def autograding_successfully_imported() -> bool:
+    """Check if autograding module is successfully imported.
+
+    If a submodule is imported (e.g. autograding.case), autograding may be imported
+    as a namespace module.
+    Namespace modules have __file__ attribute set tot None, so we can use that to check
+    if autograding was imported as a module and not just a namespace module.
+    """
+    return (
+        "autograding" in locals()
+        and locals().get("autograding").__file__
+    )
+
+# Force refresh of autograding module from upstream
+fetch_autograding()
+
+# autograding submodule might not be successfully fetched on init
+# if unsuccessful, we have to fetch it manually
+while not autograding_successfully_imported():
+    try:
+        import autograding
+        reload(autograding)
+        from autograding.case import FuncCall, InOut, RecursiveCall
+    except (ImportError, ModuleNotFoundError):
+        fetch_autograding()
+        retries += 1
+    else:
+        break
+    if retries >= MAX_RETRIES:
+        sys.exit("[import autograding] Too many retries, exiting")
 
 
 class TestSciNotation(autograding.TestInputOutput):
